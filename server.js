1fa1163e34c3a66e36729d27d5d74f90630b29fc
Mac <mac@ruslans-mbp.mynetworksettings.com>
2025-02-26 11:21:42 -0500

    changed index.php to index.js, add node.js server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'styles')));
+
+
+
+app.use('/img', express.static(path.join(__dirname, 'img')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
