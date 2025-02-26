72dfe8228e85b243bff0fe0586ea9a5a39b085d9
Mac <mac@ruslans-mbp.mynetworksettings.com>
2025-02-26 11:33:42 -0500

    api

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'src')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.static(path.join(__dirname, '..', 'styles')));
 
 app.use('/img', express.static(path.join(__dirname, '..', 'img')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'src', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = (req, res) => {
